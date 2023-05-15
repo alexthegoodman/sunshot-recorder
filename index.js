984b7cebae80c484c3f747456b53a0aadeb4f589
Alex Goodman <alexthegoodman@gmail.com>
2023-05-15 18:49:23 -0400

    test dirname

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // module.exports = require('bindings')('addon');
-// module.exports = require('node-gyp-build')(__dirname)
-module.exports = require('node-gyp-build')("../sunshot-recorder")
+module.exports = require('node-gyp-build')(__dirname)
+// module.exports = require('node-gyp-build')("../sunshot-recorder")
