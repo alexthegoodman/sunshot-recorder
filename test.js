18210d92d5cbd061ab32a03431c3693edbb916ef
Alex Goodman <alexthegoodman@gmail.com>
2023-09-09 15:20:26 -0400

    title

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ const assert = require('assert');
 const { print, setTargetWindow } = require('./');
 
 console.log(print("test"));
-console.info(setTargetWindow())
+console.info(setTargetWindow("Windows PowerShell"))
